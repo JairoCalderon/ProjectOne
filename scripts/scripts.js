c38c29f9e1553b11782ed31e0c3dd052b4dec2ec
JairoCalderon <j.calderoneng@gmail.com>
2022-03-29 10:05:12 -0400

    done with js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -12,7 +12,7 @@ formEl.addEventListener(`submit`, function (event) {
 
     function todayDate() {
 
-        const dateFormatation = {
+        const dateFormat = {
             weekday: "long",
             month: "long",
             day: "2-digit",
@@ -20,60 +20,30 @@ formEl.addEventListener(`submit`, function (event) {
         };
 
         const currentDate = new Date();
-        return currentDate.toLocaleDateString("en-US", dateFormatation)
+        return currentDate.toLocaleDateString("en-US", dateFormat)
     }
 
+    const newUser = document.createElement("div")
+    newUser.classList.add("user1")
 
-    const newComment = document.querySelector(".user1");
-    console.log(newComment)
+    const oldUser = document.querySelector(".user1")
 
-    newComment.innerHTML = `
-            <div class="userImgContainer">
-                <img src="https://picsum.photos/125/125" alt="profile head picture of lastest user">
-            </div>
-            <div class="userCommentContainer">
-                <p>${todayDate()} by ${userName}</p>
-                <p>${userCommentContent}</p>
-            </div>`;
+    oldUser.before(newUser)
 
-    // document.querySelector("#userCommentContent").appendChild(userCommentContent)
+    newUser.innerHTML = `
+        <div class="userImgContainer">
+            <img src="https://picsum.photos/125/125" alt="profile head picture of lastest user">
+        </div>
+        <div class="userCommentContainer">
+            <p id="userHeadline"></p>
+            <p id="userBodyParagraph"></p>
+        </div>`
 
+    const userHeadline = document.querySelector("#userHeadline")
+    userHeadline.textContent = `${todayDate()} by ${userName}`
 
-    // const newUserImg = document.createElement("img")
-    // newUserImg.setAttribute(`src`, `https://picsum.photos/500/500`)
-    // newUserImg.setAttribute(`alt`, `profile head picture of latest user`)
-
-
-    // const dateNameParagraph = document.createElement("p")
-    // dateNameParagraph = (`todayDate by userName}`)
-    // const paragraph = document.createElement("p")
-    // paragraph.textContent = userCommentContent
-
-
-
-    // const newUserImgContainer = document.createElement("div")
-    // newUserImgContainer.setAttribute(`class`, `userImgContainer`)
-
-    // const newUserCommentContainer = document.createElement("div")
-    // newUserCommentContainer.setAttribute(`class`, `userCommentContainer`)
-
-
-    // document.querySelector(newUserImgContainer).appendChild(newUserImg)
-
-    // document.querySelector(newUserCommentContainer).appendChild(dateNameParagraph)
-    // document.querySelector(newUserCommentContainer).appendChild(paragraph)
-
-
-    // const newUser = document.createElement("div")
-    // newUser.setAttribute(`class`, `user1`)
-
-    // document.querySelector(newUser).appendChild(newUserImgContainer)
-    // document.querySelector(newUser).appendChild(newUserCommentContainer)
-
-    // document.querySelector(`.comments`).prepend(newUser)
-
-
-
+    const userBodyParagraph = document.querySelector("#userBodyParagraph")
+    userBodyParagraph.textContent = userCommentContent
 
 })
 
